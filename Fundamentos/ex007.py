e12fe1aab6d9df5cf90d0d6950450c6c304e1a91
Davi Marques <davimarques22558800@gmail.com>
2024-08-24 20:41:50 -0300

    Conversor de medida e tabuada simples

---
+++ Fundamentos/ex007.py
@@ -0,0 +1,5 @@
+n1=float(input('Digite a primeira nota: '))
+n2=float(input('Digite a segunnda nota: '))
+media= (n1+n2)/2
+
+print(f'A média do aluno é {media:.1f}')
---
+++ Fundamentos/ex008.py
@@ -0,0 +1,10 @@
+m=float(input('Digite uma medida em metros: '))
+print(f'A medida de {m}m corresponde a:')
+print(m/1000,'km')
+print(m/100,'hm')
+print(m/10,'dam')
+print(m*10,'dm')
+print(m*100,'cm')
+print(m*1000,'mm')
+
+
