ff8b6e0832b5b109d27d445900d1930b31819f10
Davi Marques <davimarques22558800@gmail.com>
2024-09-13 18:56:43 -0300

    biblioteca random

---
+++ Fundamentos/ex020.py
@@ -0,0 +1,9 @@
+import random
+al1=input('Aluno 1: ')
+al2=input('Aluno 2: ')
+al3=input('Aluno 3: ')
+al4=input('Aluno 4: ')
+lista=[al1,al2,al3,al4]
+random.shuffle(lista)
+print(f'A ordem de apresentação é `{lista}')
+
